3e7cb6a9f09d97aeedd3056f788ac42266fc134f
James Stubbington <jamesrstubbington@gmail.com>
2020-06-16 12:07:30 -0500

    Updated ResponseUtils to Handle Entire Entity Conversion
    * Overloaded the convertToResponseEntity method to accept, single, lists, or paged model as parameters as well as the classes that they will be converted to. Can probably remove the generic ResponseContainer<T> class, but will leave it for now

--- src/main/java/org/example/todo/common/util/ResponseUtils.java
+++ src/main/java/org/example/todo/common/util/ResponseUtils.java
@@ -4,11 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.data.domain.Page;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public final class ResponseUtils {
 
+	//TODO: Investigate if ResponseContainer<T> class can be entirely removed now
 	private static final ModelMapper modelMapper = new ModelMapper();
 
 	static {
@@ -19,9 +21,9 @@ public final class ResponseUtils {
 		throw new IllegalStateException("Utility class");
 	}
 
-	public static <R, T> ResponseContainer<T> pageToDtoResponseContainer(Page<R> page, Class<T> clazz) {
+	public static <R, T, D> D convertToDtoResponseContainer(Page<R> page, Class<T> dtoClazz, Class<D> responseClazz) {
 		List<R> elements = page.getContent();
-		List<T> dtoElements = convertToDtoList(elements, clazz);
+		List<T> dtoElements = convertToDtoList(elements, dtoClazz);
 		ResponseContainer<T> responseContainer = new ResponseContainer<>(true, null, dtoElements);
 		responseContainer.setTotalElements(page.getTotalElements());
 		responseContainer.setTotalPages(page.getTotalPages());
@@ -29,18 +31,29 @@ public final class ResponseUtils {
 		responseContainer.setPage(page.getPageable().getPageNumber());
 		responseContainer.setPageSize(page.getPageable().getPageSize());
 
-		return responseContainer;
+		return convertToDto(responseContainer, responseClazz);
 	}
 
-	public static <R, T> ResponseContainer<T> pageToDtoResponseContainer(List<R> elements, Class<T> clazz) {
-		List<T> dtoElements = convertToDtoList(elements, clazz);
+	public static <R, T, D> D convertToDtoResponseContainer(List<R> elements, Class<T> dtoClazz, Class<D> responseClazz) {
+		List<T> dtoElements = convertToDtoList(elements, dtoClazz);
 		ResponseContainer<T> responseContainer = new ResponseContainer<>(true, null, dtoElements);
 		responseContainer.setTotalElements(dtoElements.size());
 		responseContainer.setTotalPages(1L);
 		responseContainer.setLast(true);
 		responseContainer.setPage(0L);
 
-		return responseContainer;
+		return convertToDto(responseContainer, responseClazz);
+	}
+
+	public static <R, T, D> D convertToDtoResponseContainer(R element, Class<T> clazz, Class<D> responseClazz) {
+		List<T> dtoElements = Collections.singletonList(convertToDto(element, clazz));
+		ResponseContainer<T> responseContainer = new ResponseContainer<>(true, null, dtoElements);
+		responseContainer.setTotalElements(dtoElements.size());
+		responseContainer.setTotalPages(1L);
+		responseContainer.setLast(true);
+		responseContainer.setPage(0L);
+
+		return convertToDto(responseContainer, responseClazz);
 	}
 
 	public static <R, T> List<T> convertToDtoList(List<R> objects, Class<T> clazz) {
